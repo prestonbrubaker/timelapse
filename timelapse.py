0cf29c2e379774798fdcfdb4da0b869687a9f901
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-01-21 01:11:21 -0600

    Update timelapse.py

--- timelapse.py
+++ timelapse.py
@@ -3,13 +3,15 @@ import time
 import os
 import RPi.GPIO as GPIO
 
-switch_pin = 11
-led_pin = 12
+switch_pin = 11    
+led_pin = 12    # LED to show if the camera takes a photo
+led_pin_2 = 13    # LED to show if the switch is open
 
 time.sleep(5)
 
 GPIO.setmode(GPIO.BOARD)      # use PHYSICAL GPIO Numbering
 GPIO.setup(led_pin, GPIO.OUT)   # set ledPin to OUTPUT mode
+GPIO.setup(led_pin_2, GPIO.OUT)   # set ledPin to OUTPUT mode
 GPIO.setup(switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # set buttonPin to PULL UP INPUT mode
 
 def capture_photo():
@@ -37,31 +39,37 @@ def capture_photo():
         int_time += 5
 
         if GPIO.input(switch_pin)==GPIO.LOW: # if button is pressed
-            #GPIO.output(led_pin,GPIO.HIGH)   # turn on led
+            GPIO.output(led_pin_2,GPIO.HIGH)   # turn on led
             print ('switch closed >>>')     # print information on terminal
             switch_closed =  True
         else : # if button is relessed
-            #GPIO.output(led_pin,GPIO.LOW) # turn off led 
+            GPIO.output(led_pin_2,GPIO.LOW) # turn off led 
             print ('switch open <<<')   
             switch_closed = False
 
         
         # Check if 60 seconds have passed
-        if(int_time - ref_time >= 60 and switch_closed == False):
-            # Generate a unique filename based on current time
-            filename = f"photos/captured_image_{int(time.time())}.png"
-
-            # Capture and save the image in the 'photos' subfolder
-            camera.capture_file(filename)
-
-            # Print a message indicating the file has been saved
-            print(f"Photo captured and saved as {filename}")
+        if(int_time - ref_time >= 60):
+            
+            if(switch_closed == True):
+                # Generate a unique filename based on current time
+                filename = f"photos/captured_image_{int(time.time())}.png"
+    
+                # Capture and save the image in the 'photos' subfolder
+                camera.capture_file(filename)
+    
+                # Print a message indicating the file has been saved
+                print(f"Photo captured and saved as {filename}")
+    
+                GPIO.output(led_pin,GPIO.HIGH)   # turn on led
+                time.sleep(1)
+                GPIO.output(led_pin,GPIO.LOW) # turn off led 
+            else:
+                print("Photo capturing skipped! The switch is open!")
 
             # Subtract 60 seconds from interval time
             int_time -= 60
-            GPIO.output(led_pin,GPIO.HIGH)   # turn on led
-            time.sleep(1)
-            GPIO.output(led_pin,GPIO.LOW) # turn off led 
+            
             
 
         # Wait for 5 seconds before checking again
